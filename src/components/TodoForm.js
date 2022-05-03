9fc91fdfe1625bf803c773e55fc8fc90999f30c1
NicklasHellion <nicola.raduazzo@gmail.com>
2022-05-03 14:08:04 +0200

    Todo app first version

---
+++ src/components/TodoForm.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  //handleChange function brings the input field to blank when a todo is added
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  //handleSubmit function prevents refreshing the page when "add todo" is clicked
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //this code snippet generates a random id (from 1 to 10000) for each todo that is generated
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Add todo</button>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
